6b277682ba3150efcfeeefef59073d5dfae1f76b
captainblue2013 <admin@lanhao.name>
2018-01-12 17:14:57 +0800

    frog init 时,内部项目的询问(决定发布到哪个网关)

--- commands/init.js
+++ commands/init.js
@@ -11,6 +11,7 @@ const readline = require('readline-sync');
 
 module.exports = () => {
   let frog = analysis();
+  frog.internal = readline.keyInYN('内部服务?'.yellow);
   if(fs.existsSync(process.cwd() + '/frog.json')){
     let cover = readline.keyInYN('Over Write(覆盖原文件) ? '.red);
     if(!cover){
@@ -18,6 +19,7 @@ module.exports = () => {
       process.exit(-1);
     }
   }
+  
   fs.writeFileSync(process.cwd() + '/frog.json', JSON.stringify(frog, null, 2));
   console.log(`Done! See:${process.cwd() + '/frog.json'}`.green);
 };

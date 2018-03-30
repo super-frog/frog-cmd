29f088ae145ae33c463fd6c8a60fc9180a358e10
captainblue2013 <admin@lanhao.name>
2018-03-30 16:09:23 +0800

    gen route get_*_list

--- commands/gen.js
+++ commands/gen.js
@@ -9,6 +9,12 @@ const gen = async (argv) => {
     case 'model':
       genModel(name);
       break;
+    case 'route':
+      whatAreYouTalking(name);
+      break;
+    default:
+      console.log(`${EOL}Usage: frog gen {model|route} xxx${EOL}`); process.exit(0);
+      break;
   }
 }
 
@@ -57,35 +63,105 @@ function whatAreYouTalking(str) {
 }
 
 function getList(p) {
+
   let modelPath = `${process.cwd()}/models/${p[1]}.js`;
   if (!fs.existsSync(modelPath)) {
     throw new Error(`Model Not Exists: ${modelPath}`);
     return;
   }
   let model = require(modelPath);
-
+  // console.log(model); process.exit(0);
+  let id = model.name;
+  let Id = PY.camel(model.name, true);
   let tpl = `
- 'use strict';
- //获取Book列表
- const bookModel = require('../definitions/models/Book.gen.js');
+'use strict';
+//获取${Id}列表
+const ${Id}Model = require('../definitions/models/${Id}.gen.js');
 
- const Query = {
-  ${fields(model, 'query')}
- };
+const Query = {
+  ${((model) => {
+      let content = '';
+      for (let k in model.fieldSet) {
+        let f = model.fieldSet[k];
+        if (k === 'createTime' || k === 'updateTime') {
+          continue;
+        }
+        if (!(f.isPrimary || f.uniqIndexName !== '' || f.indexName !== '')) {
+          continue;
+        }
+        content += `// ${f.fieldComment.replace(/\s/g, '_')} []${f.rules[0]}:0,100:0 in:query
+  ${k}:${getDefaultValue(f)},
   `;
-  console.log(tpl);process.exit(0);
-}
-function fields(model, inTag = 'query') {
-  let content = '';
-  for (let k in model.fieldSet) {
-    let f = model.fieldSet[k];
-    console.log(f);process.exit(0);
-    content += `// ${f.fieldComment.replace(/\s/g, '_')} []${f.rules[0]}
-  `;
+      }
+      content += `//页码 number:1,999 in:query
+  page: 1`;
+      return content;
+    })(model)}
+};
+
+module.exports = async (Query) => {
+  const PAGE_SIZE = 10;
+
+  let sql = 'select * from \`${id}\` where 1 ';
+   ${((model) => {
+      let content = '';
+      for (let k in model.fieldSet) {
+        let f = model.fieldSet[k];
+        if (k === 'createTime' || k === 'updateTime') {
+          continue;
+        }
+        if (!(f.isPrimary || f.uniqIndexName !== '' || f.indexName !== '')) {
+          continue;
+        }
+        content += `
+  if (Query.${k}.length){
+    sql += 'and \`${f.fieldName}\` in ('+JSON.stringify(Query.${k})+') ';
   }
-  return content;
+`;
+      }
+      return content;
+    })(model)}
+
+  sql += 'order by id desc limit ' + (Query.page - 1) * PAGE_SIZE + ',' + PAGE_SIZE;
+  let count = await ${Id}Model.raw(sql.replace('*', 'count(*) c'), {}, false);
+  if (count.length) {
+    count = count[0].c;
+  } else {
+    count = 0;
+  }
+  let list = await ${Id}Model.raw(sql, {});
+
+  return {
+    list: list,
+    total: count,
+    page: Query.page,
+    pageSize: PAGE_SIZE
+  };
+};
+  `;
+  fs.writeFileSync(`${process.cwd()}/handlers/${p[0]}.js`, tpl);
+}
+
+function getDefaultValue(fieldDefinition) {
+  let type = fieldDefinition.rules[0];
+  switch (type) {
+    case 'string':
+      return `'${fieldDefinition.defaultValue || ''}'`;
+      break;
+    case 'enum':
+    case 'number':
+      return `${fieldDefinition.defaultValue || 0}`;
+      break;
+    case 'array':
+      return `['${fieldDefinition.defaultValue.join('\',\'')}']`;
+      break;
+    case 'ref':
+      return `new ${fieldDefinition.type.ref.name}({})`;
+      break;
+    default:
+      return `''`;
+      break;
+  }
 }
 
 module.exports = gen
-//todo
-//whatAreYouTalking('get_book_list')
